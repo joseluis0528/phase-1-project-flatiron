150975b84d406f514efaf1ee0521f8ac784e1a18
Jose <joseluis0528@gmail.com>
2023-03-30 23:34:15 -0500

    added defer attribute

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
-    
+    getDate()
 })
 
 function fetch() {
     
+}
+
+function getDate () {
+    const date = new Date()
+    const day = date.getDate()
+    const month = date.getMonth() + 1
+    const year = date.getFullYear()
+    document.querySelector('.date').innerText = `${month}/${day}/${year}`
 }
