c461e4f27c19c61a02ed55ae32d69878609280bb
Jose <joseluis0528@gmail.com>
2023-04-06 20:37:31 -0500

    added second fetch request

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
 function fetch(city) {
     let apiKey = "GZ46xqn7DcyEqd0L0Xr1ApK2KuJh2NcG"
+    fetch(`"http://dataservice.accuweather.com/locations/v1/cities/search?apikey=GZ46xqn7DcyEqd0L0Xr1ApK2KuJh2NcG&q=${city}&language=en-us&details=true"`)
+    .then(response => response.json())
+    .then(data => data.key)
+
     fetch(`http://dataservice.accuweather.com/forecasts/v1/daily/1day/348308?apikey=${apiKey}&language=en-us&details=true&metric=true`)
     .then(responce => responce.json())
     .then(data => data)
