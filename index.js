ef9eb84ec3544e6c9282fbeffddade1aaf5c511d
Jose <joseluis0528@gmail.com>
2023-04-06 20:43:17 -0500

    update second fetch request

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
 function fetch(city) {
     let apiKey = "GZ46xqn7DcyEqd0L0Xr1ApK2KuJh2NcG"
-    fetch(`"http://dataservice.accuweather.com/locations/v1/cities/search?apikey=GZ46xqn7DcyEqd0L0Xr1ApK2KuJh2NcG&q=${city}&language=en-us&details=true"`)
+    const getKey = fetch(`"http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${apiKey}&q=${city}&language=en-us&details=true"`)
     .then(response => response.json())
     .then(data => data.key)
 
-    fetch(`http://dataservice.accuweather.com/forecasts/v1/daily/1day/348308?apikey=${apiKey}&language=en-us&details=true&metric=true`)
+    fetch(`http://dataservice.accuweather.com/forecasts/v1/daily/1day/${getKey}?apikey=${apiKey}&language=en-us&details=true&metric=true`)
     .then(responce => responce.json())
     .then(data => data)
 }
